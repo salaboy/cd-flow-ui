24156e7be9885c31efab16951a482d968865c250
salaboy <salaboy@gmail.com>
2020-12-29 11:36:36 +0100

    adding projects component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
 import './App.scss';
+import React from "react";
+import Projects from './Pages/Projects'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/environments">Environments</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/environments">
+            <Environments />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
   );
 }
 
-export default App;
+
+
+function Project() {
+  // The <Route> that rendered this component has a
+  // path of `/projects/:projectId`. The `:projectId` portion
+  // of the URL indicates a placeholder that we can
+  // get from `useParams()`.
+  let { projectId } = useParams();
+  let { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <h3>{projectId}</h3>
+      <ul>
+        <li>
+          <Link to={`${url}/modulea`}>Module A</Link>
+        </li>
+        <li>
+          <Link to={`${url}/moduleb`}>Module B</Link>
+        </li>
+        
+      </ul>
+
+      <Switch>
+        <Route exact path={path}>
+          <h3>Please select a Module.</h3>
+        </Route>
+        <Route path={`${path}/:moduleId`}>
+          <Module />
+        </Route>
+      </Switch>
+    </div>
+    
+  );
+}
+
+function Module() {
+  // The <Route> that rendered this component has a
+  // path of `/modules/:moduleId`. The `:moduleId` portion
+  // of the URL indicates a placeholder that we can
+  // get from `useParams()`.
+  let { moduleId } = useParams();
+
+  return (
+    <div>
+      <h3>{moduleId}</h3>
+    </div>
+  );
+
+}
+
+function Environments() {
+  return (
+    <div>
+      <h2>Environments</h2>
+    </div>
+  );
+}
---
+++ src/Pages/Projects/index.js
@@ -0,0 +1,20 @@
+import React, {
+    useParams,
+    Component,
+    Switch,
+  Route,
+  Link
+} from 'react';
+
+export default class Projects extends Component {
+    render() {
+        
+        return (
+            <div>
+                <h2>Projects</h2>
+                
+
+            </div>
+        );
+    }
+}
