f366ef738cb7a5df8c20f959c8e95bbe020fc169
Ezequiel <ezequiel.salatino@gmail.com>
2021-01-17 13:02:45 +0100

    More type, sections and grid

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -19,20 +19,32 @@ export default class Home extends Component {
 
       return (
         <div>
-            <h1>The romantic in me wants to imagine there was no flash, no bang.</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium non accusantium, atque minima aliquid dolores. Perspiciatis dicta accusamus laborum ipsam omnis harum nihil quidem, autem corporis, voluptate nulla, debitis reprehenderit.</p>
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro laboriosam deleniti iusto nisi! Itaque accusamus, obcaecati. Ab nobis iusto alias, debitis sit magni maxime quae distinctio consequuntur animi, expedita et!</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro laboriosam deleniti iusto nisi! Itaque accusamus, obcaecati. Ab nobis iusto alias, debitis sit magni maxime quae distinctio consequuntur animi, expedita et!</p>
-            <h3>Itaque accusamus, obcaecati. Ab nobis iusto alias.</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro laboriosam deleniti iusto nisi! Itaque accusamus, obcaecati. Ab nobis iusto alias, debitis sit magni maxime quae distinctio consequuntur animi, expedita et!</p>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro laboriosam deleniti iusto nisi! Itaque accusamus, obcaecati. Ab nobis iusto alias, debitis sit magni maxime quae distinctio consequuntur animi, expedita et!</p>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h5>
-            <p className="p p-s">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro laboriosam deleniti iusto nisi! Itaque accusamus, obcaecati. Ab nobis iusto alias, debitis sit magni maxime quae distinctio consequuntur animi, expedita et!</p>
-            <p className="p p-s">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro laboriosam deleniti iusto nisi! Itaque accusamus, obcaecati. Ab nobis iusto alias, debitis sit magni maxime quae distinctio consequuntur animi, expedita et!</p>
-            <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h6>
-            <p className="p p-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro laboriosam deleniti iusto nisi! Itaque accusamus, obcaecati. Ab nobis iusto alias, debitis sit magni maxime quae distinctio consequuntur animi, expedita et!</p>
+          <div className="container">
+            <div className="col col-eight">
+              <div className="section">
+                <h1>The romantic in me wants to imagine there was no flash, no bang.</h1>
+                <p className="p p-b">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium non accusantium, atque minima aliquid dolores. Perspiciatis dicta accusamus laborum ipsam omnis harum nihil quidem, autem corporis, voluptate nulla, debitis reprehenderit.</p>
+                <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro laboriosam deleniti iusto nisi! Itaque accusamus, obcaecati. Ab nobis iusto alias, debitis sit magni maxime quae distinctio consequuntur animi, expedita et!</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro laboriosam deleniti iusto nisi! Itaque accusamus, obcaecati. Ab nobis iusto alias, debitis sit magni maxime quae distinctio consequuntur animi, expedita et!</p>
+              </div>
+              <div className="section">
+                <h3>Itaque accusamus, obcaecati. Ab nobis iusto alias.</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro laboriosam deleniti iusto nisi! Itaque accusamus, obcaecati. Ab nobis iusto alias, debitis sit magni maxime quae distinctio consequuntur animi, expedita et!</p>
+                <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro laboriosam deleniti iusto nisi! Itaque accusamus, obcaecati. Ab nobis iusto alias, debitis sit magni maxime quae distinctio consequuntur animi, expedita et!</p>
+              </div>
+            </div>
+            <div className="col col-four">
+              <div className="section">
+                <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h5>
+                <p className="p p-s">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro laboriosam deleniti iusto nisi! Itaque accusamus, obcaecati. Ab nobis iusto alias, debitis sit magni maxime quae distinctio consequuntur animi, expedita et!</p>
+                <p className="p p-s">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro laboriosam deleniti iusto nisi! Itaque accusamus, obcaecati. Ab nobis iusto alias, debitis sit magni maxime quae distinctio consequuntur animi, expedita et!</p>
+                <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h6>
+                <p className="p p-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro laboriosam deleniti iusto nisi! Itaque accusamus, obcaecati. Ab nobis iusto alias, debitis sit magni maxime quae distinctio consequuntur animi, expedita et!</p>
+              </div>
+            </div>
+          </div>
         </div>
       );
     }
